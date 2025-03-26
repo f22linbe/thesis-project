b64dade17a81ace78bc3fa393fe93460d8cb04c5
f22linbe <f22linbe@student.his.se>
2025-03-26 14:50:25 +0100

    Fastify, Implemented schema, connection to database, async route to books

--- fastify-api/server.js
+++ fastify-api/server.js
@@ -1,7 +1,35 @@
-// Import the framework and instantiate it
 import Fastify from 'fastify'
+import FastifyEnv from '@fastify/env'
+import pg from '@fastify/postgres'
+
 const fastify = Fastify({
   logger: true
+})
+
+// Set Environment
+const schema = {
+  type: 'object',
+  required: ['PORT', 'DATABASE_URL'],
+  properties: {
+    PORT: {
+      type: 'string',
+      default: 3000
+    },
+    DATABASE_URL: {
+      type: 'string'
+    }
+  }
+}
+
+await fastify.register(FastifyEnv, {
+  schema: schema,
+  dotenv: true,
+  confKey: 'config',
+})
+
+// Connect to database
+await fastify.register(pg,{
+  connectionString: fastify.config.DATABASE_URL
 })
 
 // Declare a route
@@ -9,9 +37,23 @@ fastify.get('/', async function handler (request, reply) {
   return { hello: 'world' }
 })
 
+fastify.get('/book/:id', async (req,reply) => {
+  const client = await fastify.pg.connect()
+  try {
+    const { rows } = await client.query(
+      'SELECT text FROM books WHERE id=$1', [req.params.id]
+    )
+    return rows
+  } finally {
+    // Release the client immediately after query resolves
+    client.release()
+  }
+
+})
+
 // Run the server!
 try {
-  await fastify.listen({ port: 3000 })
+  await fastify.listen({port: fastify.config.PORT})
 } catch (err) {
   fastify.log.error(err)
   process.exit(1)
