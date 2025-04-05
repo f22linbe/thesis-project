64a3202728a9e61dff739ff83cb4a6f7946e1e05
f22linbe <f22linbe@student.his.se>
2025-04-05 18:07:53 +0200

    Fixed bug, incorrect return type of bookdata to return a object instead of a string

--- gin-api/handlers/bookhandler.go
+++ gin-api/handlers/bookhandler.go
@@ -22,12 +22,12 @@ func GetBookHandler(c *gin.Context) {
 		return
 	}
 	// Get book data
-	booktext, err := repository.GetBook(bookid)
+	bookdata, err := repository.GetBook(bookid)
 	if err != nil {
 		c.JSON(404, gin.H{"error": "Book was not found"})
 		return
 	}
-	c.JSON(200, gin.H{"text": booktext})
+	c.JSON(http.StatusOK, bookdata)
 }
 
 func PostBookHandler(c *gin.Context) {
