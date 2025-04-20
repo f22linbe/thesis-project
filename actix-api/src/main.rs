0f7a4de1c7641ac324b081ddb8e08c638b93b97f
f22linbe <f22linbe@student.his.se>
2025-04-20 17:58:53 +0200

    Add tracing for logging to identify the cause of the bottleneck, change sqlx runtime feature and other dependencies some may still be unnecessary

--- actix-api/src/main.rs
+++ actix-api/src/main.rs
@@ -4,6 +4,7 @@ use actix_web::{
 };
 use dotenv::dotenv;
 use sqlx::{Pool, Postgres, postgres::PgPoolOptions};
+use tracing_subscriber::FmtSubscriber;
 mod services;
 use services::{create_book, fetch_book, greet};
 pub struct AppState {
@@ -12,6 +13,13 @@ pub struct AppState {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let subscriber = FmtSubscriber::builder()
+        .with_env_filter("debug")
+        .with_target(false)
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber).expect("setting tracing subscriber failed");
+
     dotenv().ok();
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let dbpool = PgPoolOptions::new()
