4322de24754dcc6aac0c957fb875bddbb73bf1a2
f22linbe <f22linbe@student.his.se>
2025-04-22 01:58:00 +0200

    Update generateText() generate string instead of characters took too long

--- K6/test.js
+++ K6/test.js
@@ -1,6 +1,8 @@
 import http from "k6/http";
 import { sleep } from "k6";
+import { check } from "k6";
 
+// NOT IN USE (USED IN generateText())
 function mulberry32(a) {
   return function () {
     var t = (a += 0x6d2b79f5);
@@ -9,35 +11,43 @@ function mulberry32(a) {
     return ((t ^ (t >>> 14)) >>> 0) / 4294967296;
   };
 }
-
+// NOT IN USE (TOO TIME CONSUMING)
 function generateText(size, seed) {
   let text = "";
   const target = size * 1024;
   // Characters
-  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz ";
+  // const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz ";
 
   // Generate text to target
+  // while (text.length < target) {
+  //   text += characters.charAt(Math.floor(seed() * characters.length));
+  // }
+
   while (text.length < target) {
-    text += characters.charAt(Math.floor(seed() * characters.length));
+    text += seed().toString(36);
   }
 
   return text;
 }
+const numOfUsers = 2; // 2, 5, 10, 20
+const numOfIterations = 10; // 250, 100, 50, 25
 
 export const options = {
   scenarios: {
     contacts: {
       executor: "per-vu-iterations",
-      vus: 2,
-      iterations: 10,
-      // maxDuration: "30s",
+      vus: numOfUsers,
+      iterations: numOfIterations,
+      maxDuration: "2h",
     },
   },
 };
 
 export default function () {
   const url = "http://localhost:3003/gin/book"; // (/:3001/actix, /:3002/fastify, /:3003/gin )
-  const seed = 10 - __ITER;
+  const index = __ITER * numOfUsers + (__VU - 1);
+  const seed = index + 1;
+  console.log(`index: ${index}`);
   // Initiate seed
   const mySeed = mulberry32(seed);
   const payload = JSON.stringify({
@@ -45,13 +55,25 @@ export default function () {
     book_text: generateText(500, mySeed),
     book_size: "small", // small = 500, medium = 1000, large = 1500
   });
-
+  console.log(typeof smallText);
+  console.log(smallText.length);
   const header = {
     headers: {
       "Content-Type": "application/json",
     },
   };
 
-  http.post(url, payload, header);
+  const res = http.post(url, payload, header);
+
+  check(res, { "is status 201": (r) => r.status === 201 });
+  console.log(`Response status: ${res.status}`);
+  console.log(`Response body: ${res.body}`);
   sleep(1);
+}
+export function handleSummary(data) {
+  console.log("Summary:");
+  console.log(JSON.stringify(data.metrics, null, 2));
+  return {
+    "summary.json": JSON.stringify(data, null, 2),
+  };
 }
